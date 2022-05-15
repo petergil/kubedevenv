21fa02170e36af07f4305547ec5c16e5606da3d3
Peter Gille <petergil@absum.me>
2022-05-15 17:46:07 +0200

    Initial commit

---
+++ local-settings.sh
@@ -0,0 +1,10 @@
+
+
+## Uncomment this line if you want to run the setup using podman.
+## If you want to run podman as rootless, also see
+## https://kind.sigs.k8s.io/docs/user/rootless/
+#KIND_EXPERIMENTAL_PROVIDER=podman
+
+## Uncomment this line and set it to a value if you want to mount a
+## different directory on the host inside of the cluster
+#KIND_LOCAL_STORAGE=
